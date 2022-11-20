9c925815c702f9d51f883c5897d29ac25c04b99a
InformationWarrior <102821977+InformationWarrior@users.noreply.github.com>
2022-11-20 19:20:34 +0530

    Update 236A - Boy or Girl.cs

--- 236A - Boy or Girl.cs
+++ 236A - Boy or Girl.cs
@@ -1,3 +1,5 @@
+//Solution 1
+
 using System;
 class Program
 {
@@ -20,3 +22,35 @@ class Program
         
     }
 }
+
+
+//Solution 2
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        string s1 = Console.ReadLine();
+        char[] ch = s1.ToCharArray();
+        // Array.Sort(ch);
+
+        List<char> li = new List<char>();
+
+        for (int i = 0; i < ch.Length; i++)
+        {
+            if (!li.Contains(ch[i]))
+            {
+                li.Add(ch[i]);
+            }
+        }
+        if (li.Count % 2 != 0)
+            Console.WriteLine("IGNORE HIM!");
+        else
+            Console.WriteLine("CHAT WITH HER!");
+
+    }
+}
+    

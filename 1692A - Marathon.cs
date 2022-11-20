045e8b6422368fab9b351ab165e1b5a09e1dbee2
InformationWarrior <102821977+InformationWarrior@users.noreply.github.com>
2022-11-20 19:43:04 +0530

    Update 1692A - Marathon.cs

--- 1692A - Marathon.cs
+++ 1692A - Marathon.cs
@@ -49,3 +49,27 @@ class Program
         }
     }
 }
+
+//Solution 3
+
+using static System.Console;
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;         
+            int[] a = Array.ConvertAll(ReadLine().Split(), int.Parse);
+            if (a[1] > a[0])
+                count++;
+            if (a[2] > a[0])
+                count++;
+            if (a[3] > a[0])
+                count++;
+            WriteLine(count);
+        }
+    }
+}

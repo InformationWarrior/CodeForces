44f93e8fee12c519588bd37d5cd4219bef210cb2
InformationWarrior <102821977+InformationWarrior@users.noreply.github.com>
2022-12-02 20:06:25 +0530

    Update 141A - Amusing Joke.cs

--- 141A - Amusing Joke.cs
+++ 141A - Amusing Joke.cs
@@ -1,3 +1,5 @@
+//Method 1
+
 using System;
 using System.Linq;
 class Program
@@ -25,5 +27,47 @@ class Program
         char[] characters = input.ToArray();
         Array.Sort(characters);        
         return new string(characters);
+    }
+}
+
+//Method 2
+
+using System;
+class Program
+{
+    public static void Main()
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        string c = Console.ReadLine();
+
+        int[] arr1 = new int[500];
+        int[] arr2 = new int[500];
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            arr1[a[i]]++;
+        }
+
+        for (int i = 0; i < b.Length; i++)
+        {
+            arr1[b[i]]++;
+        }
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            arr2[c[i]]++;
+        }
+
+        string ans = "YES";
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                ans = "NO";
+                break;
+            }
+        }
+        Console.WriteLine(ans);
     }
 }
